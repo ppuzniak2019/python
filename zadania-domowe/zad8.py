db2ca65b8a604a666b8a45992a836cb5fbe2ba3c
p-puzniak <PawelPuzniak2019>
2019-11-24 19:03:30 +0100

    zrobiony program, działa

--- zadania-domowe/zad8.py
+++ zadania-domowe/zad8.py
@@ -1,1 +1,17 @@
 #Za pomocą with open( ) proszę odczytać zawartość jednego pliku i zapisać w drugim. Proszę uwzględnić obsługę wyjątków.
+
+#Najpierw tworzymy plik 1 i coś w nim zapisujemy:
+plik_tekstowy = open('plik1.txt', 'w')
+plik_tekstowy.write("Testujemy kopiowanie plików")
+plik_tekstowy.close()
+
+#Za pomocą with otwieramy pliki i przenosimy dane.
+try:
+    with open("plik1.txt") as p1:
+        tekst = p1.readlines()
+        with open("plik2.txt", "w") as p2:
+            p2.writelines(tekst)
+except (IOError) as e:
+    print(e)
+
+
